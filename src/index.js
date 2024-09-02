50c910d0c92ffe5b75068220e09738fb000f62e9
Yogeswaran Thulasiyan <73667144+SyThulasi@users.noreply.github.com>
2024-09-02 23:36:26 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import AngleSelector from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AngleSelector />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
